b1ddcbe8925804fd6cae4f161849d79dfed2eba3
Emirhan Gocturk <emirgoppa@gmail.com>
2020-01-22 20:11:21 +0100

    Engine name and version in the title

--- TopDownShooter/Properties/AssemblyInfo.cs
+++ TopDownShooter/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TopDownShooter")]
+[assembly: AssemblyTitle("TopDownShooter - DreamVoyage Engine Version 0.0.1")]
 [assembly: AssemblyProduct("TopDownShooter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
