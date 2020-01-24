fe4479c982de70b601519010727d8d5602e1a3dc
Emirhan Gocturk <emirgoppa@gmail.com>
2020-01-24 18:09:53 +0100

    Name change

--- TopDownShooter/Properties/AssemblyInfo.cs
+++ TopDownShooter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DreamVoyage Engine - Build Version 0.3.0")]
-[assembly: AssemblyProduct("TopDownShooter")]
+[assembly: AssemblyProduct("DreamVoyage")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
